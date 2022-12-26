c78657a510c14ce1cc70feddd580a602313e715d
JakubKub13 <jakubkubala3@gmail.com>
2022-12-26 11:28:02 +0100

    Fixing bugs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const { read, realpath } = require('fs');
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY
@@ -12,24 +13,36 @@ const readline = require("readline").createInterface({
 });
 
 async function main() {
-    readline.question("Ask anything (type q to quit): ", async (prompt) => {
-        if (prompt === "q") {
-            readline.close();
-            return;
-        }
+    let continueAsking = true;
 
-        readline.question("Enter the max number of tokens(length of output): ", async (max_tokens) => {
-            const response = await AIbrain.createCompletion({
-                model: "text-davinci-003",
-                prompt: prompt,
-                max_tokens: parseInt(max_tokens),
-                temperature: 0,
+    while (continueAsking) {
+    
+        readline.question("Ask anything (type q to quit): ", async (prompt) => {
+            if (prompt === "q") {
+                readline.close();
+                return;
+            }
+
+            readline.question("Enter the max number of tokens(length of output): ", async (max_tokens) => {
+                const response = await AIbrain.createCompletion({
+                    model: "text-davinci-003",
+                    prompt: prompt,
+                    max_tokens: parseInt(max_tokens),
+                    temperature: 0,
+                });
+
+                const completion = response.data.choices[0].text;
+                console.log(completion);
+
+                readline.question("Do you want to continue asking questions? (y/n): ", (continueResponse) => {
+                    if (continueResponse === "n") {
+                        continueAsking = false;
+                        readline.close();
+                    }
+                });
             });
-
-            const completion = response.data.choices[0].text;
-            console.log(completion);
-        });
-    }); 
+        }); 
+    }
 }
 
 main();
