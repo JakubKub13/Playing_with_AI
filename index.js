561b64520e7580c472f51cf4426226b03cdd5e7e
JakubKub13 <jakubkubala3@gmail.com>
2022-12-26 10:15:46 +0100

    Async operations

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY
 });
 const AIbrain = new OpenAIApi(configuration);
-const response = await AIbrain.createCompletion({
-    model: "text-davinci-003",
-    prompt: "Say this is a test",
-    max_tokens: 7,
-    temperature: 0,
-});
+
+async function main() {
+    const response = await AIbrain.createCompletion({
+        model: "text-davinci-003",
+        prompt: "Say this is a test",
+        max_tokens: 7,
+        temperature: 0,
+    });
+
+    console.log(response)
+}
+
+main()
